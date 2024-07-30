1a580f832cffcb7a7f50a7c2a9c0344029f8ac17
Anass Bouatra <anass.bouatra1@gmail.com>
2024-07-30 09:05:14 +0300

    merged fixed

--- backend/insert_test.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username, password, role) VALUES ('admin', 'admin_password_hash', 'admin');
-INSERT INTO users (username, password, role) VALUES ('reception', 'reception_password_hash', 'reception');
-SELECT * FROM analyses WHERE id = 1;
-SELECT * FROM echantillons WHERE id = (SELECT echantillon_id FROM analyses WHERE id = 1);
-SELECT * FROM elementsdinteret WHERE analysis_id = 1;
