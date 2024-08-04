e82da71ae7ed7337ac4c531369606ce25bf390fb
Abdelajlil Farid <abdeljalilfaride@gmail.com>
2024-08-04 13:36:49 +0300

    suprimer ancien code

--- backend/database/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Fonctions réutilisables
-// Exemple de fonction pour nettoyer les données entrées par l'utilisateur
-function cleanInput($input) {
-    // Utiliser les fonctions de nettoyage nécessaires (éviter les injections SQL, etc.)
-    return htmlspecialchars(strip_tags(trim($input)));
-}
-?>
