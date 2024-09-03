ab3c51889c49d01e341c0b4dbaab6dec2a516b9b
AhmedTaha3 <bekahmad36@gmail.com>
2024-09-03 11:47:34 +0300

    forme de fiche technique

--- frontend/src/components/reception/FicheTechnique/FicheTechnique.js
+++ frontend/src/components/reception/FicheTechnique/FicheTechnique.js
@@ -56,7 +56,7 @@ const FicheTechnique = () => {
 
     const generatePDF = () => {
         const input = ficheRef.current;
-        const scale = 3; // Augmentez cette valeur pour une meilleure qualité
+        const scale = 3;
         html2canvas(input, { scale: scale })
             .then(canvas => {
                 const imgData = canvas.toDataURL('image/png');
@@ -64,7 +64,28 @@ const FicheTechnique = () => {
                 const imgProps = pdf.getImageProperties(imgData);
                 const pdfWidth = pdf.internal.pageSize.getWidth();
                 const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-                pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+                
+                let startY = 0;
+
+                demandes.forEach((demande, index) => {
+                    if (index > 0) {
+                        pdf.addPage();
+                        startY = 0;
+                    }
+
+                    pdf.addImage(imgData, 'PNG', 0, startY, pdfWidth, pdfHeight);
+                    startY += pdfHeight;
+
+                    // Generate additional pages for samples if needed
+                    const samplesPerPage = 2;
+                    const totalPages = Math.ceil(demande.echantillons.length / samplesPerPage);
+
+                    for (let page = 1; page < totalPages; page++) {
+                        pdf.addPage();
+                        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+                    }
+                });
+
                 pdf.save(`fichetechnique_${generateClientReference(clientId, year, month)}.pdf`);
             })
             .catch(error => console.error('Erreur lors de la génération du PDF', error));
@@ -76,6 +97,7 @@ const FicheTechnique = () => {
 
     return (
         <div>
+            <button onClick={generatePDF} className="generate-pdf-button">Télécharger en PDF</button>
             <div ref={ficheRef}>
                 {demandes && demandes.length > 0 ? (
                     demandes.map((demande, demandeIndex) => (
@@ -83,17 +105,17 @@ const FicheTechnique = () => {
                             
                             <div className='container-ft'>
                                 <div className='header-ft-elements'>
-                                <div className='header-ft'>
-                                    <div className='instn-logo'>
-                                        <img src={instnlogo} alt="INSTN Logo" />
+                                    <div className='header-ft'>
+                                        <div className='instn-logo'>
+                                            <img src={instnlogo} alt="INSTN Logo" />
+                                        </div>
+                                        <h3>Ministère de l'Enseignement Supérieur et de la Recherche Scientifique</h3>
+                                        <h3>INSTITUT NATIONAL DES SCIENCES ET TECHNIQUES NUCLEAIRES</h3>
+                                        <h3>(INSTN-Madagascar)</h3>
+                                        <h3>B.P.3907</h3>
+                                        <h3>Tél: 0321179224</h3>
+                                        <h3>E-mail : instn@moov.mg</h3>
                                     </div>
-                                    <h3>Ministère de l'Enseignement Supérieur et de la Recherche Scientifique</h3>
-                                    <h3>INSTITUT NATIONAL DES SCIENCES ET TECHNIQUES NUCLEAIRES</h3>
-                                    <h3>(INSTN-Madagascar)</h3>
-                                    <h3>B.P.3907</h3>
-                                    <h3>Tél: 0321179224</h3>
-                                    <h3>E-mail : instn@moov.mg</h3>
-                                </div>
                                 </div>
                                 <div className='title-ft'>
                                     <h2>FICHE TECHNIQUE DE SERVICE</h2>
@@ -101,6 +123,7 @@ const FicheTechnique = () => {
                                 <div className='form-header-ft'>
                                 <p>Informations personnelles</p>
                                 </div>
+                                
                                 <div className='info-perso'>
                                     <div className="form-group-ft">
                                         <label>Référence:</label>
@@ -114,6 +137,8 @@ const FicheTechnique = () => {
                                         <label>Adresse:</label>
                                         <p>{demande.address}</p>
                                     </div>
+                                 </div>
+                                 <div className='info-perso'>
                                     <div className="form-group-ft">
                                         <label>Téléphone:</label>
                                         <p>{demande.phone}</p>
@@ -179,11 +204,12 @@ const FicheTechnique = () => {
                                                     <label>Observations:</label>
                                                     <p>{echantillon.sampleObservations}</p>
                                                 </div>
+                                                <label>Détails sur les analyses</label>
                                                 {echantillon.analyses.map((analyse, analyseIndex) => (
-                                                    <div key={analyseIndex} className="analysis-section">
+                                                    <div key={analyseIndex} >
                                                         <div className="form-group-ft">
-                                                            <label>Details sur l'analayse {analyseIndex+1}:</label>
-                                                            <p>Analyse {analyse.analysisType} de {analyse.parameter} par {analyse.technique} pour les elements : {analyse.elementsDinteret.map(e => e.elementDinteret).join(', ')}</p>
+                                                            {/* <label>Details sur l'analayse {analyseIndex+1}:</label> */}
+                                                            <li>Analyse {analyse.analysisType} de {analyse.parameter} par {analyse.technique} pour les elements : {analyse.elementsDinteret.map(e => e.elementDinteret).join(', ')}</li>
                                                         </div>
                                                     </div>
                                                 ))}
@@ -193,20 +219,22 @@ const FicheTechnique = () => {
                                         
                                     ))}
                                 </div>
-                                <div className="form-header-ft">
-                                            <h2>Delais de livraison</h2>
-                                </div>
-                                <div className="form-group-ft">
-                                            <label>Date de livraison:</label>
-                                            <p>{demande.dilevery_delay}</p>
-                                </div>
-                                <div className="form-header-ft">
-                                            <h2>Signatures</h2>
-                                </div>
-                                <div className='signatures'>
-                                    <p>Réceptionnaire,</p>
-                                    <p>Visa de la Direction,</p>
-                                    <p>Signature du client,</p>
+                                <div className='footer'>
+                                    <div className="form-header-ft">
+                                                <h2>Delais de livraison</h2>
+                                    </div>
+                                    <div className="form-group-ft">
+                                                <label>Date de livraison:</label>
+                                                <p>{demande.dilevery_delay}</p>
+                                    </div>
+                                    <div className="form-header-ft">
+                                                <h2>Signatures</h2>
+                                    </div>
+                                    <div className='signatures'>
+                                        <p>Réceptionnaire,</p>
+                                        <p>Visa de la Direction,</p>
+                                        <p>Signature du client,</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -215,7 +243,7 @@ const FicheTechnique = () => {
                     <div className="no-details">Aucun détail disponible.</div>
                 )}
             </div>
-            <button onClick={generatePDF} className="generate-pdf-button">Télécharger en PDF</button>
+           
         </div>
     );
 };
